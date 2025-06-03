64ec62154390cfb3e54441f618b812ed979f6fdc
Frane177 <golem.frane12@gmail.com>
2025-06-04 00:53:11 +0200

    fixed small game announcer text, changed because because of repo name change

--- src/components/gameAnnouncement.jsx
+++ src/components/gameAnnouncement.jsx
@@ -3,7 +3,7 @@ export default function GameAnnouncement({isWin, winner, replayAction}) {
         <div className="game-announcement-wrapper">
             <div className="game-announcement bg-light text-dark">
                 {isWin ? 
-                    <h1 style={{textAlign:"center"}}>{winner.name} je pobijedio!</h1> : 
+                    <h1 style={{textAlign:"center"}}>{winner.name} je pobijedio/la!</h1> : 
                     <h1 style={{textAlign:"center"}}>Izjednačeno!</h1>}
                 <div style={{display:"flex", justifyContent:"center"}}>
                     <button className="button-play" onClick={e => {replayAction();}}>Igraj ponovno!</button>
